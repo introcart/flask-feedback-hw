982444d6e7878a7065135479eae955215173cdb2
Steven Carton <spatc@protonmail.com>
2021-01-26 17:32:56 -0500

    Adding Feedback to User Template

--- models.py
+++ models.py
@@ -52,3 +52,5 @@ class Feedback(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     username = db.Column(db.String(20), db.ForeignKey('users.username'))
+
+    user = db.relationship('User', backref="feedbacks")
